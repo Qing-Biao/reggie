cb8360358f7c522768f55da87115be4ec3a852c5
xie <2821293903@qq.com>
2024-02-25 22:51:08 +0800

    使用Redis缓存短信验证码和菜品数据

--- src/main/java/com/jxufe/reggie/controller/UserController.java
+++ src/main/java/com/jxufe/reggie/controller/UserController.java
@@ -2,12 +2,14 @@ package com.jxufe.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.jxufe.reggie.common.R;
+import com.jxufe.reggie.config.RedisConfig;
 import com.jxufe.reggie.pojo.User;
 import com.jxufe.reggie.service.UserService;
 import com.jxufe.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author Xie
@@ -31,6 +34,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 用户登录
      *
@@ -48,7 +53,10 @@ public class UserController {
         String code = map.get("code").toString();
 
         //从Session中获取保存的验证码
-        Object codeInsession = session.getAttribute(phone);
+       // Object codeInsession = session.getAttribute(phone);
+
+        //从redis中获取缓存的验证码
+        Object codeInsession = redisTemplate.opsForValue().get(phone);
 
         //进行验证码比对(页面提交的验证码和session中保存的验证码比对)
         if (codeInsession != null && codeInsession.equals(code)) {
@@ -65,6 +73,7 @@ public class UserController {
             }
             session.setAttribute("user", user.getId());
 
+            //如果用户登录成功，删除Redis中缓存的验证码
             return R.success(user);
         }
         return R.error("登录失败");
@@ -99,7 +108,10 @@ public class UserController {
 
 
             //将生成的验证码保存到Session
-            session.setAttribute(phone,code);
+            //session.setAttribute(phone,code);
+
+            //将生成的验证码缓存到Redis中，并设置有效期为5分钟
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
 
             return R.success("手机发送验证码成功");
         }
